9736e83e831176ebe0513ef249d4a1b4e2ee483e
Ritika <ritikaguleria51@gmail.com>
2026-09-04 10:17:48 +0530

    new

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,16 +4,20 @@ import { PromoGridSection } from "../components/HomeData/PromoGrid/PromoGridSect
 import { TopCollectionBanner } from "../components/HomeData/TopCollection/TopCollectionBanner";
 import FeaturedOffers from "../components/HomeData/Features/FeaturedOffers";
 import Banner1 from "../components/HomeData/Banner1/Banner1";
+import Feature1 from "../components/HomeData/Feature1/Feature1";
+import NearbySection from "../components/HomeData/NearbySection/NearbySection";
 function Home(){
     return(
         <>
 
             <section className="home">
                 <Banner1 />
+                <Feature1 />
                 <Hero/>
                 <PromoGridSection />
                 <TopCollectionBanner/>
                 <FeaturedOffers />
+                <NearbySection />
             </section>
         </>
     );
